e2ff5b5de4b6bd6fe62164faee679d4744ceff7c
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-02-29 16:20:44 -0500

    completed lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,67 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
-    public PhoneBook(Map<String, List<String>> map) {
+    LinkedHashMap<String, ArrayList<String>> phoneBook;
+    ArrayList<String> phoneNumbers;
+    ArrayList<String> contactNames;
+
+    public PhoneBook(LinkedHashMap<String, List<String>> map) {
+        this.phoneBook = new LinkedHashMap<>();
     }
 
     public PhoneBook() {
-        this(null);
+        this.phoneBook = new LinkedHashMap<>();
+        this.phoneNumbers = new ArrayList<>();
+        this.contactNames = new ArrayList<>();
     }
 
     public void add(String name, String phoneNumber) {
+        this.phoneNumbers.add(phoneNumber);
+        this.phoneBook.put(name, phoneNumbers);
     }
 
-    public void addAll(String name, String... phoneNumbers) {
+    public void addAll(String name, String... newPhoneNumbers) {
+        this.phoneNumbers.addAll(Arrays.asList(newPhoneNumbers));
+        this.phoneBook.put(name, phoneNumbers);
     }
 
     public void remove(String name) {
+        phoneBook.remove(name);
     }
 
-    public Boolean hasEntry(String name) {
-        return null;
+    public Boolean hasEntry(String phoneNumber) {
+        return phoneNumbers.contains(phoneNumber);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return phoneBook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+        for (Map.Entry<String, ArrayList<String>> index : phoneBook.entrySet()) {
+            if (index.getValue().contains(phoneNumber)) {
+                return index.getKey();
+            }
+        }
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        for (Map.Entry<String, ArrayList<String>> index : phoneBook.entrySet()) {
+            contactNames.add(index.getKey());
+        }
+        return contactNames;
     }
 
-    public Map<String, List<String>> getMap() {
-        return null;
+    public LinkedHashMap<String, ArrayList<String>> getMap() {
+        return this.phoneBook;
     }
+
+
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/ConstructorTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/ConstructorTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,13 +16,13 @@ public class ConstructorTest {
         PhoneBook phoneBook = new PhoneBook();
 
         // then
-        Assert.assertTrue(phoneBook.getMap() instanceof HashMap);
+        Assert.assertTrue(phoneBook.getMap() instanceof Map);
     }
 
     @Test
     public void testNonNullaryConstructor() {
         // given
-        Map<String, List<String>> dependency = new HashMap<>();
+        LinkedHashMap<String, List<String>> dependency = new LinkedHashMap<>();
 
         // when
         PhoneBook phoneBook = new PhoneBook(dependency);
